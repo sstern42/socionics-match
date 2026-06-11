50b6cac8483c1fe2bf99ca073f6208407e1fe525
Spencer Stern <spencer.stern@gmail.com>
2026-06-11 11:45:09 +0100

    Delete supabase/verified_badge_migration.sql

--- supabase/verified_badge_migration.sql
+++
@@ -1,5 +0,0 @@
--- Add verified_by column to users table
--- Stores the name of the person who verified the type (e.g. 'Spencer', 'Uncle_Sam')
--- Null = unverified
-
-alter table users add column if not exists verified_by text default null;
